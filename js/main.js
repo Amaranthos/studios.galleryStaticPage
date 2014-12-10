32d7d914e72732d3d29699d402b33f2b7c1a7ddc
manpat <pat@patsown.com>
2014-12-10 20:32:44 +1000

    Grid page

--- js/main.js
+++ js/main.js
@@ -2,40 +2,40 @@ function contact(){
 	alert("No. We don't like you");
 }
 
-function addThingToSlidyThing(head, captionhead, caption){
-	var slidyThing = document.getElementById("myCarousel");
-	{
-		var indicatorsCon = slidyThing.getElementsByClassName("carousel-indicators")[0]
-		var count = indicatorsCon.children.length;
-		var element = document.createElement("li");
-		element.setAttribute("data-target", "#myCarousel");
-		element.setAttribute("data-slide-to", count);
-		indicatorsCon.appendChild(element);
-	}
+// function addThingToSlidyThing(head, captionhead, caption){
+// 	var slidyThing = document.getElementById("myCarousel");
+// 	{
+// 		var indicatorsCon = slidyThing.getElementsByClassName("carousel-indicators")[0]
+// 		var count = indicatorsCon.children.length;
+// 		var element = document.createElement("li");
+// 		element.setAttribute("data-target", "#myCarousel");
+// 		element.setAttribute("data-slide-to", count);
+// 		indicatorsCon.appendChild(element);
+// 	}
 
-	var slidesCon = slidyThing.getElementsByClassName("carousel-inner")[0];
-	var element = document.createElement("div");
-	element.className = "item";
+// 	var slidesCon = slidyThing.getElementsByClassName("carousel-inner")[0];
+// 	var element = document.createElement("div");
+// 	element.className = "item";
 
-	var h2 = document.createElement("h2");
-	h2.innerHTML = head;
-	element.appendChild(h2);
+// 	var h2 = document.createElement("h2");
+// 	h2.innerHTML = head;
+// 	element.appendChild(h2);
 
-	var cap = document.createElement("div");
-	cap.className = "carousel-caption";
+// 	var cap = document.createElement("div");
+// 	cap.className = "carousel-caption";
 
-	var ch3 = document.createElement("h3");
-	ch3.innerHTML = captionhead;
-	cap.appendChild(ch3);
+// 	var ch3 = document.createElement("h3");
+// 	ch3.innerHTML = captionhead;
+// 	cap.appendChild(ch3);
 
-	var cp = document.createElement("p");
-	cp.innerHTML = caption;
-	cap.appendChild(cp);
+// 	var cp = document.createElement("p");
+// 	cp.innerHTML = caption;
+// 	cap.appendChild(cp);
 
-	element.appendChild(cap);
+// 	element.appendChild(cap);
 
-	slidesCon.appendChild(element);
-}
+// 	slidesCon.appendChild(element);
+// }
 
 
-addThingToSlidyThing("Vad?", "Värför är detta här", "Jag vet inte. Vet du?");
+// addThingToSlidyThing("Vad?", "Värför är detta här", "Jag vet inte. Vet du?");
