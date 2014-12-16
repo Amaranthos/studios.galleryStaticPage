0e2b20541f0b6ba871c849904c5e6d2ed6a15d77
Silcoish <silcoish@gmail.com>
2014-12-16 22:43:55 +1000

    Sliders, Description, About Us
    Added all the images to the sliders, with links to their pages.
    Fixed the description of the website on the main page
    Created an about us page to outline a little about us and to give
    credits where due

--- js/index.js
+++ js/index.js
@@ -44,5 +44,17 @@ function addThingToSlidyThing(head, captionhead, caption, background, page){
 }
 
 
-addThingToSlidyThing("Vad?", "Varför är detta här", "Jag vet inte. Vet du?", "img/samuel.png", "game1.html");
-addThingToSlidyThing("What?", "Why's this here?", "I blame tony abbott", "img/awesomegamelogo.png", "game2.html");
+addThingToSlidyThing("Cube Infinity", "", "Get circles! Three in a row increases your size and multiplier. Avoid triangles! Three will take your multiplier and 25 points.", "img/Slider/CubeInfinity.png", "game1.html");
+addThingToSlidyThing("Highway Chase", "", "Speed down the highway to catch the escaping villain in your Austin martin db5. !!Explosions!!", "img/Slider/Highway.png", "game2.html");
+addThingToSlidyThing("Cannon Kings", "", "Build a castle to protect your king from the devastating fury of the enemies cannon fire. Hit the enemy king with a cannon ball to win.", "img/Slider/CannonKings.png", "game3.html");
+
+addThingToSlidyThing("Renovo", "", "You have stolen forbidden magic from the high mages, and have been sent to a labyrinth called Renovo as punishment.", "img/Slider/Renovo.png", "game11.html");
+addThingToSlidyThing("Walter's Wizard Warehouse", "", "Can you solve the puzzles and lead Walter out of Walters Wizard Warehouse.", "img/Slider/WWW.png", "game12.html");
+addThingToSlidyThing("Rizard Whythm", "", "Hear the Music, be the music.", "img/Slider/Rizard.png", "game13.html");
+
+addThingToSlidyThing("Get Kraken", "", "Prevent tourists from disrupting your quiet fishing spot so that you can release the kraken.", "img/Slider/GetKraken.png", "game21.html");
+addThingToSlidyThing("Antivirus Altercation", "", "Everyone knows that antivirus programs play Bomberman in their spare time!", "img/Slider/AntiVirus.png", "game22.html");
+addThingToSlidyThing("Valour", "", "This is Valour, a 2D side-scrolling platform game based on the aftermath of Normandy Beach from World War 2.", "img/Slider/Valour.png", "game23.html");
+addThingToSlidyThing("Escape from Dimension 67", "", "How did you end up here? Too much science! Harvest sheep on this pixellated island if you want any hope of surviving the night.", "img/Slider/67.png", "game24.html");
+addThingToSlidyThing("Galileo", "", "You are alone on an alien world and your ship is out of fuel. Find the compound you need to fuel your ship and get home.", "img/Slider/Galileo.png", "game25.html");
+addThingToSlidyThing("Deadwood Tune-Up", "", "Fight through waves of zombies while you collect parts to fix for your broken down car. Collect by day, fight by night.", "img/Slider/DeadWood.png", "game26.html");
